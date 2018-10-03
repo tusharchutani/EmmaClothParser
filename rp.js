0c22f8f2457210b02b0dd6cfcbf79c98badbf958
Tushar Chutani <tushar_chutani@yahoo.com>
2018-10-03 16:43:26 -0700

    Adding data

--- rp.js
+++ rp.js
@@ -5,14 +5,15 @@ const sleep = require('util').promisify(setTimeout)
 
 module.exports = class RP{
     static async rp(options){
+        log.info("Trying to connect to "+options.uri); 
         var data;
         try{
             data = await rp(options)
         }catch(error){
-            log.error("There is an error in calling getAllProductNameAndUrl info "+error);
+            log.error("There is an error in calling info "+error+ " URL:"+options.uri);
             log.info("Trying in 5 seconds again"); 
-            await sleep(5000)
-            data = rp(options);
+            await sleep(5000);
+            data = await rp(options);
         }
         return data
     }
